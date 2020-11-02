8052adeccfd9fba8fe50830f5a9e391efde12e97
andrew2020wit <andrew2020wit@gmail.com>
2020-11-02 16:51:03 +0200

    vuejs-first-app

---
+++ vuejs/vuejs-first-app/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <div v-bind:class="`block1`">
+      <Introduction />
+    </div>
+  </div>
+</template>
+
+<script>
+import Introduction from "./components/introduction/introduction";
+
+export default {
+  name: "App",
+  components: {
+    Introduction,
+  },
+};
+</script>
+
+<style>
+.block1 {
+  margin: 10px;
+  padding: 10px;
+  border: 1px solid black;
+}
+</style>
