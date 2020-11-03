ba3c793a6fdec97075af8c5008545c6cd1840211
andrew2020wit <andrew2020wit@gmail.com>
2020-11-03 18:38:53 +0200

    Vuex: state.count

--- vuejs/vue-poligon/src/router/index.js
+++ vuejs/vue-poligon/src/router/index.js
@@ -1,5 +1,6 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
+import Counter from "../views/counter";
 import Home from "../views/Home.vue";
 
 Vue.use(VueRouter);
@@ -8,7 +9,12 @@ const routes = [
   {
     path: "/",
     name: "Home",
-    component: Home
+    component: Home,
+  },
+  {
+    path: "/counter",
+    name: "Counter",
+    component: Counter,
   },
   {
     path: "/about",
@@ -17,14 +23,14 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
-  }
+      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+  },
 ];
 
 const router = new VueRouter({
   mode: "history",
   base: process.env.BASE_URL,
-  routes
+  routes,
 });
 
 export default router;
---
+++ vuejs/vue-poligon/src/views/counter.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <p>Counter: {{ count }}</p>
+    <p>
+      <button @click="increment">+</button>
+      <button @click="decrement">-</button>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Counter",
+  props: {},
+  methods: {
+    increment() {
+      this.$store.commit("increment");
+    },
+    decrement() {
+      this.$store.commit("decrement");
+    },
+  },
+  computed: {
+    count() {
+      return this.$store.state.count;
+    },
+  },
+};
+</script>
+
+<style scoped></style>
