7d46d7c4a8d7359e2b3737c4b127997484b42ae1
andrew2020wit <andrew2020wit@gmail.com>
2020-11-12 08:31:54 +0200

    del

--- webpack/webpack-poligon/src/analytics.js
+++
@@ -1,7 +0,0 @@
-count = 0;
-
-document.addEventListener("click", () => count++);
-
-setInterval(() => {
-  console.log("analytics.js, click-count: ", count);
-}, 1000);
--- webpack/webpack-poligon/src/lib.js
+++
@@ -1,5 +0,0 @@
-export function getHello(x) {
-  const r = `Hello, ${x}!`;
-  console.log(r);
-  return r;
-}
